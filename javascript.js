6177e28b8f9e180f117cc1f11356b5e9f8c29715
Straw17 <strawmcstick@gmail.com>
2020-04-12 23:28:17 -0400

    new Streetball mode and new moods for Botto

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function bottoMood() {
-    var randomNumber = Math.floor((Math.random() * 7) + 1);
+    var randomNumber = Math.floor((Math.random() * 12) + 1);
     var imgName = "botto" + randomNumber + ".png";
     document.body.style.backgroundImage = 'url(Images/Botto/' + imgName + ')';
 }
@@ -8,6 +8,7 @@ function streetball() {
     var paddleHeight = 150;
     var paddleWidth = 10;
     var ballRadius = 25;
+    var maxSpeedOfPaddle = 10;
     var speedOfPaddle1 = 0;
     var positionOfPaddle1 = 460;
     var speedOfPaddle2 = 0;
@@ -19,9 +20,11 @@ function streetball() {
     var score1 = 0;
     var score2 = 0;
     var sides = [-1, 1];
+    var friction = 0.3;
 
     var isPractice;
     var name;
+    var hasPsychics;
     var ballAcceleration;
     var initBallSpeed;
 
@@ -30,6 +33,7 @@ function streetball() {
     function startBall() {
         isPractice = document.getElementById("practiceBool").innerHTML;
         name = document.getElementById("name").innerHTML;
+        hasPsychics = document.getElementById("psychics").innerHTML;
         ballAcceleration = document.getElementById("ballAcceleration").innerHTML;
         initBallSpeed = document.getElementById("initBallSpeed").innerHTML;
         topPositionOfBall = window.innerHeight / 2;
@@ -45,11 +49,13 @@ function streetball() {
         if (e.keyCode == 83 || e.which == 83) { //S key
             speedOfPaddle1 += 10;
         }
-        if (e.keyCode == 38 || e.which == 38) { //up arrow
-            speedOfPaddle2 -= 10;
-        }
-        if (e.keyCode == 40 || e.which == 40) { //down arrow
-            speedOfPaddle2 += 10;
+        if(isPractice != "true") {
+            if (e.keyCode == 38 || e.which == 38) { //up arrow
+                speedOfPaddle2 -= 10;
+            }
+            if (e.keyCode == 40 || e.which == 40) { //down arrow
+                speedOfPaddle2 += 10;
+            }
         }
     }, false);
 
@@ -91,21 +97,31 @@ function streetball() {
     observer2.observe(target2, config2);
 
     window.setInterval(function show() {
+        /*if(Math.abs(speedOfPaddle1) > maxSpeedOfPaddle) {
+            speedOfPaddle1 = maxSpeedOfPaddle * (speedOfPaddle1/Math.abs(speedOfPaddle1));
+        }
+        if(Math.abs(speedOfPaddle2) > maxSpeedOfPaddle) {
+            speedOfPaddle2 = maxSpeedOfPaddle * (speedOfPaddle2/Math.abs(speedOfPaddle2));
+        }*/
         if (document.activeElement.id != "modes") {
             positionOfPaddle1 += speedOfPaddle1;
             positionOfPaddle2 += speedOfPaddle2;
 
             if (positionOfPaddle1 <= 100) {
                 positionOfPaddle1 = 100;
+                speedOfPaddle1 = 0;
             }
             if (positionOfPaddle2 <= 100) {
                 positionOfPaddle2 = 100;
+                speedOfPaddle2 = 0;
             }
             if (positionOfPaddle1 >= window.innerHeight - paddleHeight) {
                 positionOfPaddle1 = window.innerHeight - paddleHeight;
+                speedOfPaddle1 = 0;
             }
             if (positionOfPaddle2 >= window.innerHeight - paddleHeight) {
                 positionOfPaddle2 = window.innerHeight - paddleHeight;
+                speedOfPaddle2 = 0;
             }
 
             document.getElementById('paddle1').style.top = (positionOfPaddle1) + "px";
@@ -119,6 +135,9 @@ function streetball() {
             }
             if (leftPositionOfBall <= paddleWidth - 2) {
                 if (topPositionOfBall > positionOfPaddle1 && topPositionOfBall < positionOfPaddle1 + paddleHeight) {
+                    if(hasPsychics == "true") {
+                        topSpeedOfBall += (speedOfPaddle1*friction);
+                    }
                     leftSpeedOfBall = -leftSpeedOfBall * ballAcceleration;
                 } else {
                     score2 += 1;
@@ -127,6 +146,9 @@ function streetball() {
             }
             if (leftPositionOfBall >= window.innerWidth - ballRadius - paddleWidth + 2) {
                 if ((topPositionOfBall > positionOfPaddle2 && topPositionOfBall < positionOfPaddle2 + paddleHeight) || (isPractice=="true")) {
+                    if(hasPsychics == "true") {
+                        topSpeedOfBall += (speedOfPaddle2*friction);
+                    }
                     leftSpeedOfBall = -leftSpeedOfBall * ballAcceleration;
                 } else {
                     score1 += 1;
@@ -146,11 +168,20 @@ function streetball() {
 function streetballClassic() {
     document.getElementById("name").innerHTML = "classic";
     document.getElementById("ballAcceleration").innerHTML = 1.15;
-    document.getElementById("initBallSpeed").innerHTML = 5;
+    document.getElementById("initBallSpeed").innerHTML = 5.00;
+    document.getElementById("psychics").innerHTML = false;
 }
 
 function streetballSpeedy() {
     document.getElementById("name").innerHTML = "speedy";
-    document.getElementById("ballAcceleration").innerHTML = 1;
-    document.getElementById("initBallSpeed").innerHTML = 10;
+    document.getElementById("ballAcceleration").innerHTML = 1.00;
+    document.getElementById("initBallSpeed").innerHTML = 10.00;
+    document.getElementById("psychics").innerHTML = false;
+}
+
+function streetballPsychic() {
+    document.getElementById("name").innerHTML = "psychic";
+    document.getElementById("ballAcceleration").innerHTML = 1.00;
+    document.getElementById("initBallSpeed").innerHTML = 7.50;
+    document.getElementById("psychics").innerHTML = true;
 }
